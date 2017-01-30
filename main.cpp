4f4b49255786f2ffb10744c134290c9cdbc90e85
Nicholas Bering <bering.nicholas@gmail.com>
2017-01-29 19:13:25 -0500

    Some basic aruco marker recognition.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 #include "opencv2/aruco.hpp"
 
 #include <iostream>
@@ -10,18 +11,73 @@ using namespace std;
 int main()
 {
     Mat markerImage;
-    Ptr<Dictionary> dictionary = getPredefinedDictionary(DICT_6X6_250);
+    int keyCode;
+    int markerId = 1;
+    bool done = false;
 
-    drawMarker(dictionary, 25, 200, markerImage, 1);
+    Ptr<Dictionary> dictionary = getPredefinedDictionary(DICT_5X5_1000);
+    Ptr<DetectorParameters> detectorParams = DetectorParameters::create();
 
+    VideoCapture inputVideo;
+    inputVideo.open(0);
 
-    namedWindow("MyWindow", CV_WINDOW_AUTOSIZE);
+    namedWindow("Marker", CV_WINDOW_AUTOSIZE);
+    namedWindow("Camera", CV_WINDOW_AUTOSIZE);
 
-    imshow("MyWindow", markerImage);
+    while(!done && inputVideo.grab())
+    {
+        Mat image, imageCopy;
+        
+        inputVideo.retrieve(image);
 
-    waitKey(0);
+        vector< int > ids;
+        vector< vector< Point2f > > corners, rejected;
+        vector< Vec3d > rvecs, tvecs;
 
-    destroyWindow("MyWindow");
+        detectMarkers(image, dictionary, corners, ids, detectorParams, rejected);
+
+        image.copyTo(imageCopy);
+
+        if (ids.size() > 0)
+        {
+            drawDetectedMarkers(imageCopy, corners, ids);
+        }
+
+        if (ids.size() == 2)
+        {
+            cv::rectangle(imageCopy, corners[0][0], corners[1][0], Scalar(100, 0, 255));
+        }
+
+        imshow("Camera", imageCopy);
+
+        drawMarker(dictionary, markerId, 200, markerImage, 1);
+
+        imshow("Marker", markerImage);
+
+        keyCode = waitKey(1000);
+
+        switch(keyCode)
+        {
+            case 123 :
+                markerId = markerId > 0 ? markerId - 1 : 0;
+                break;
+            case 124 :
+                markerId = markerId < 999 ? markerId + 1 : 999;
+                break;
+            case 125 :
+                markerId = markerId > 0 ? markerId - 1 : 0;
+                break;
+            case 126 :
+                markerId = markerId < 999 ? markerId + 1 : 999;
+                break;
+            case 27 :
+                done = true;
+                break;
+        }
+    }
+
+    destroyWindow("Marker");
+    destroyWindow("Camera");
 
     return 0;
 }
